dfc858c3556dc078119161112d4a1db2c336e472
TheCaptain1810 <itspranavpise@gmail.com>
2025-11-13 17:53:54 +0530

    setup for react might be done!

---
+++ choices.js
@@ -0,0 +1,29 @@
+export const appKindChoices = [
+  { name: "Frontend", value: "Frontend" },
+  { name: "Backend", value: "Backend" },
+  { name: "Full Stack", value: "Full Stack" },
+  { name: "Mobile", value: "Mobile" },
+  { name: "Desktop", value: "Desktop" },
+];
+
+export const frontendFrameworkChoices = [
+  { name: "Vanilla", value: "Vanilla" },
+  { name: "React+Vite", value: "React" },
+  { name: "Vue.js", value: "Vue.js" },
+  { name: "Angular", value: "Angular" },
+];
+
+export const backendLanguageChoices = [
+  { name: "Node.js", value: "Node.js" },
+  { name: "Python", value: "Python" },
+  { name: "Java", value: "Java" },
+  { name: "C#", value: "C#" },
+  { name: "PHP", value: "PHP" },
+  { name: "Ruby", value: "Ruby" },
+];
+
+export const nodejsFrameworkChoices = [
+  { name: "Vanilla (Express)", value: "Vanilla" },
+  { name: "Nest.js", value: "Nest.js" },
+  { name: "Fastify", value: "Fastify" },
+];
--- index.js
+++ index.js
@@ -1,15 +1,90 @@
 #!/usr/bin/env node
 
 import fs from "fs";
+import { exec } from "child_process";
+import { select, Separator } from "@inquirer/prompts";
+import {
+  appKindChoices,
+  frontendFrameworkChoices,
+  backendLanguageChoices,
+  nodejsFrameworkChoices,
+} from "./choices.js";
 
-const projectStructure = {
-  src: ["index.js"],
-  public: ["index.html", "styles.css"],
-};
-
-Object.entries(projectStructure).forEach(([dir, files]) => {
-  fs.mkdirSync(dir, { recursive: true });
-  files.forEach((file) => fs.writeFileSync(`${dir}/${file}`, ""));
+const appKind = await select({
+  message: "What king of application do you want to build?",
+  choices: appKindChoices,
 });
+
+switch (appKind) {
+  case "Frontend":
+    const framework = await select({
+      message: "What frontend framework would you like to use?",
+      choices: frontendFrameworkChoices,
+    });
+    switch (framework) {
+      case "React":
+        const language = await select({
+          message: "Would you like to use TypeScript?",
+          choices: [
+            { name: "Yes", value: "Yes" },
+            { name: "No", value: "No" },
+          ],
+        });
+        if (language === "Yes") {
+          exec(
+            "npm create vite@latest ./ -- --template react-ts",
+            (error, stdout, stderr) => {
+              if (error) {
+                console.error(`Error: ${error.message}`);
+                return;
+              }
+              if (stderr) {
+                console.error(`Stderr: ${stderr}`);
+                return;
+              }
+            }
+          );
+        } else {
+          exec(
+            "npm create vite@latest ./ -- --template react",
+            (error, stdout, stderr) => {
+              if (error) {
+                console.error(`Error: ${error.message}`);
+                return;
+              }
+              if (stderr) {
+                console.error(`Stderr: ${stderr}`);
+                return;
+              }
+            }
+          );
+        }
+    }
+    break;
+  case "Backend":
+    const language = await select({
+      message: "What backend language would you like to use?",
+      choices: backendLanguageChoices,
+    });
+    switch (language) {
+      case "Node.js":
+        const framework = await select({
+          message: "Which Node.js backend framework would you like to use?",
+          choices: nodejsFrameworkChoices,
+        });
+        switch (framework) {
+          case "Vanilla":
+            const config = await select({
+              message: "Provide other config", // TODO
+              choices: [{}],
+            });
+            break;
+        }
+        break;
+    }
+    break;
+  default:
+    console.log("Please choose valid options");
+}
 
 console.log("Project structure created successfully.");
